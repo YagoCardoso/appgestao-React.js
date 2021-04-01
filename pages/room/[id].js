2aa30565f55f3c686b078618e82080e6d8b39faf
Yago Cardoso <48061155+YagoCardoso@users.noreply.github.com>
2021-04-01 00:47:43 -0300

    refactoring function submit cadSala

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -30,14 +30,45 @@ const router = useRouter();
 const classes = useStyles();
 const id = router.query.id;
 const [values, setValues] = useState(initialValue);
+const [nomeSala, setName] = useState('');
 
 useEffect(() => {
-    axios.get("https://localhost:44354/api/Sala/1").then((myData) => {
+    axios.get(`https://localhost:44354/api/Sala/${router.query.id}`).then((myData) => {
 const { data } = myData;
 setSala(data);
 });
 }, []);
 
+const submitValue = () => {
+  const frmdetails = {
+      'IDSALA' : router.query.id,
+      'NOME' : nomeSala,
+  }
+  console.log(frmdetails);
+  try {
+    axios.put(`https://localhost:44354/api/Sala/${router.query.id}`, frmdetails )
+    .then(res => { console.log(res);  console.log(res.data);
+      if(res.status == 200){ 
+      alert('Alterado com sucesso');
+       
+  }
+   
+    })
+
+ } catch (error) {
+    // Error 😨
+    if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        console.log(error.request);
+    } else {
+        console.log('Error', error.message);
+    }
+    console.log(error);
+ }
+}
 
 
 function onSubimitUpdateFormSala(ev){
@@ -87,13 +118,13 @@ return (
         </DialogContentText>
         <form onSubmit={onSubimitUpdateFormSala} className={classes.root}>
               <TextField helperText="não permitido, exlcua e crie uma nova." disabled onChange ={onChange}  margin="dense" value={getSala.idsala}   name="IDSALA" label="Nº Sala" type="text"  variant="outlined" autoFocus   />
-              <TextField onChange ={onChange}  margin="dense" helperText={`Atual: ${getSala.nome}`}  name="NOME" label="Nome Sala" type="text"  variant="outlined" autoFocus   />
-         
+              <TextField  onChange={e => setName(e.target.value)}  margin="dense" helperText={`Atual: ${getSala.nome}`}  name="NOME" label="Nome Sala" type="text"  variant="outlined" autoFocus   />
+             
                {/* <input placeholder="Nº Sala" type="text" name="IDSALA" />
                <input placeholder="Nome Sala" type="text" name="NOME" /> */}
                <br></br>
                 <Button color="primary">Cancelar </Button>
-                <Button type="submit" color="primary"> Salvar </Button>
+                <Button onClick={submitValue}  color="primary"> Salvar </Button>
             </form>
           </DialogContent>
       </div>
